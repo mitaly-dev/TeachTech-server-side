fb944b307fdda7efa86edb6e7390687272567abd
mitaly-dev <mitalyy.dev@gmail.com>
2022-10-25 05:34:17 -0700

    single course item api created

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const port = process.env.PORT || 5000
 app.use(cors())
 const courses = require('./Data/courses.json')
 
-app.get('/',(req,res)=>{
-    res.send('iam running in server')
-})
-
 app.get('/courses',(req,res)=>{
     res.send(courses)
+})
+
+app.get('/courses/:id',(req,res)=>{
+    const id=req.params.id
+    const course = courses.find(singleCourse=>singleCourse.id===id)
+    res.send(course)
 })
 
 app.listen(port,()=>{
