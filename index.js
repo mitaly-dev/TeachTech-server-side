ecf22daa06e95976ec4d464b4cd2f1a5616f38c4
mitaly-dev <mitalyy.dev@gmail.com>
2022-10-25 23:53:39 -0700

    blog data create

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 const port = process.env.PORT || 5000
 app.use(cors())
 const courses = require('./Data/courses.json')
+const blogs = require('./Data/blog.json')
 
 app.get('/courses',(req,res)=>{
     res.send(courses)
@@ -13,6 +14,16 @@ app.get('/courses/:id',(req,res)=>{
     const id=req.params.id
     const course = courses.find(singleCourse=>singleCourse.id===id)
     res.send(course)
+})
+
+app.get('/primium_access/:id',(req,res)=>{
+    const id=req.params.id
+    const course = courses.find(singleCourse=>singleCourse.id===id)
+    res.send(course)
+})
+
+app.get('/blogs',(req,res)=>{
+    res.send(blogs)
 })
 
 app.listen(port,()=>{
