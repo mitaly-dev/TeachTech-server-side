1afbec2436694db8c7010b7bb857ed8c42265466
mitaly-dev <mitalyy.dev@gmail.com>
2022-10-26 03:05:02 -0700

    added some meaningful comments

--- index.js
+++ index.js
@@ -6,22 +6,26 @@ app.use(cors())
 const courses = require('./Data/courses.json')
 const blogs = require('./Data/blog.json')
 
+// all courses
 app.get('/courses',(req,res)=>{
     res.send(courses)
 })
 
+// get single course details
 app.get('/courses/:id',(req,res)=>{
     const id=req.params.id
     const course = courses.find(singleCourse=>singleCourse.id===id)
     res.send(course)
 })
 
+// get course info for primium access
 app.get('/primium_access/:id',(req,res)=>{
     const id=req.params.id
     const course = courses.find(singleCourse=>singleCourse.id===id)
     res.send(course)
 })
 
+// get all blogs data
 app.get('/blogs',(req,res)=>{
     res.send(blogs)
 })
